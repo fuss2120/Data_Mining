194cf3ebbb14fff2db4e4ae2d8127217b8a5ed9e
Michael Fuss <30630082+fuss2120@users.noreply.github.com>
2019-12-01 10:45:43 -0600

    update

--- server.R
+++ server.R
@@ -12,27 +12,52 @@ library(igraph)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+    
     output$distPlot <- renderPlot({
         
         if(input$Name){
-            wiki = read.csv(file("/Users/michaelfuss/Documents/DMP_date.csv"), header=FALSE)
+            wiki = read.csv("/Users/michaelfuss/Documents/DMP_date.csv", header=FALSE)
             relations <- data.frame(from=wiki$V1,
                                     to=wiki$V2,
                                     weights=wiki$V3,
                                     dates=wiki$V4)
             testSet <- subset(relations, relations$to==input$bins, select = c("from", "to", "weights", "dates"))
+            type=c(1, -1, 0) 
+            testSet$color[testSet$weights==-1] <- "red" 
+            testSet$color[testSet$weights==1] <- "darkgreen" 
+            testSet$color[testSet$weights==0] <- "blue" 
             g <- graph.data.frame(testSet, directed=FALSE)
-            l <- layout.fruchterman.reingold(g, niter=5000, area=vcount(g)^4*10)
+            l <- layout.fruchterman.reingold(g, niter=5000)
         }
         if(input$Date){
-            wiki = read.csv(file("/Users/michaelfuss/Documents/DMP_date.csv"), header=FALSE)
+            wiki = read.csv("/Users/michaelfuss/Documents/DMP_date.csv", header=FALSE)
             relations <- data.frame(from=wiki$V1,
                                     to=wiki$V2,
                                     weights=wiki$V3,
                                     dates=wiki$V4)
-            testSet <- subset(relations, relations$dates>input$StartDate & relations$dates<input$EndDate , select = c("from", "to", "weights", "dates"))
+            relations$dates = as.Date(relations$dates, "%m/%d/%y")
+            testSet <- subset(relations, relations$dates>=as.Date(input$StartDate) & relations$dates<=as.Date(input$EndDate) , select = c("from", "to", "weights", "dates"))
+            type=c(1, -1, 0) 
+            testSet$color[testSet$weights==-1] <- "red" 
+            testSet$color[testSet$weights==1] <- "darkgreen" 
+            testSet$color[testSet$weights==0] <- "blue" 
             g <- graph.data.frame(testSet, directed=FALSE)
-            l <- layout.fruchterman.reingold(g, niter=5000, area=vcount(g)^4*10)
+            l <- layout.fruchterman.reingold(g, niter=5000)
+        }
+        if(input$`Name and Date`){
+            wiki = read.csv("/Users/michaelfuss/Documents/DMP_date.csv", header=FALSE)
+            relations <- data.frame(from=wiki$V1,
+                                    to=wiki$V2,
+                                    weights=wiki$V3,
+                                    dates=wiki$V4)
+            relations$dates = as.Date(relations$dates, "%m/%d/%y")
+            testSet <- subset(relations, relations$to==input$bins & relations$dates>=as.Date(input$StartDate) & relations$dates<=as.Date(input$EndDate) , select = c("from", "to", "weights", "dates"))
+            type=c(1, -1, 0) 
+            testSet$color[testSet$weights==-1] <- "red" 
+            testSet$color[testSet$weights==1] <- "darkgreen" 
+            testSet$color[testSet$weights==0] <- "blue" 
+            g <- graph.data.frame(testSet, directed=FALSE)
+            l <- layout.fruchterman.reingold(g, niter=5000)
         }
 
         # generate bins based on input$bins from ui.R
